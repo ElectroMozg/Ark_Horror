c3077463f97278b8c16d311c9709a29515b3c61a
Oleg in Home <kio5885@gmail.com>
2022-07-11 23:39:32 +0700

    Описал параметры класса врага

--- src/main/java/model/Enemy.java
+++ src/main/java/model/Enemy.java
@@ -1,4 +1,28 @@
 package model;
 
+import model.component.cards.scenario.EnemyCard;
+import model.scenario.Sets;
+
+import java.util.ArrayList;
+
 public class Enemy {
+
+    EnemyCard card;
+
+    int fightValue;
+    int health;
+    int evade;
+
+    int damage;
+    int horror;
+
+    int victoryPoints;
+    Sets set;
+
+    ArrayList<String> attributes;
+
+    Location spawn;
+    Player pray;
+
 }
+
---
+++ src/main/java/model/scenario/Sets.java
@@ -0,0 +1,12 @@
+package model.scenario;
+
+public enum Sets {
+
+    THE_GATHERING,
+    RATS,
+    GHOULS,
+    STRIKINGS_FEAR,
+    ANCIENT_EVIL,
+    CHILLING_COLD
+
+}
